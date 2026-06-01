a05760ae2ff0301d3c65a38d497c0212754cf72b
MaxLananas <113060593+MaxLananas@users.noreply.github.com>
2026-06-01 10:00:48 -0400

    Update DistortionCommand.java

--- src/main/java/de/btegermany/terraplusminus/commands/DistortionCommand.java
+++ src/main/java/de/btegermany/terraplusminus/commands/DistortionCommand.java
@@ -1,125 +1,85 @@
 package de.btegermany.terraplusminus.commands;
 
-import de.btegermany.terraplusminus.Terraplusminus;
 import de.btegermany.terraplusminus.gen.RealWorldGenerator;
 import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import net.buildtheearth.terraminusminus.projection.GeographicProjection;
 import net.buildtheearth.terraminusminus.projection.OutOfProjectionBoundsException;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
+import org.bukkit.generator.ChunkGenerator;
 import org.jetbrains.annotations.NotNull;
 
 public class DistortionCommand implements BasicCommand {
 
     @Override
-    public void execute(
-            @NotNull CommandSourceStack stack,
-            @NotNull String[] args
-    ) {
+    public void execute(@NotNull CommandSourceStack stack, @NotNull String[] args) {
 
         if (!(stack.getSender() instanceof Player player)) {
             stack.getSender().sendMessage(
-                    Component.text(
-                            "This command can only be used by players.",
-                            NamedTextColor.RED
-                    )
+                    Component.text("Players only.", NamedTextColor.RED)
             );
             return;
         }
 
         if (!player.hasPermission("t+-.distortion")) {
             player.sendMessage(
-                    Component.text(
-                            "No permission for /distortion",
-                            NamedTextColor.RED
-                    )
+                    Component.text("No permission for /distortion", NamedTextColor.RED)
             );
             return;
         }
 
-        RealWorldGenerator generator =
-                Terraplusminus.instance.getActiveGenerator();
+        World world = player.getWorld();
+        ChunkGenerator generator = world.getGenerator();
 
-        if (generator == null) {
+        if (!(generator instanceof RealWorldGenerator realGen)) {
             player.sendMessage(
-                    Component.text(
-                            "No active Terra+- generator found.",
-                            NamedTextColor.RED
-                    )
+                    Component.text("This world is not using Terra+- generator.", NamedTextColor.RED)
             );
             return;
         }
+
+        GeographicProjection projection = realGen.getSettings().projection();
 
         try {
+            double[] geo = projection.toGeo(
+                    player.getLocation().getX(),
+                    player.getLocation().getZ()
+            );
 
-            double[] geo = generator.getSettings()
-                    .projection()
-                    .toGeo(
-                            player.getLocation().getX(),
-                            player.getLocation().getZ()
-                    );
+            double[] tissot = projection.tissot(
+                    geo[0],
+                    geo[1]
+            );
 
-            double[] tissot = generator.getSettings()
-                    .projection()
-                    .tissot(
-                            geo[0],
-                            geo[1],
-                            0.00001
-                    );
-
-            double distortion =
-                    Math.sqrt(Math.abs(tissot[0]));
-
-            double angle =
-                    Math.toDegrees(tissot[1]);
+            double scale = Math.sqrt(Math.abs(tissot[0]));
+            double angle = Math.toDegrees(tissot[1]);
 
             player.sendMessage(
                     Component.text()
-                            .append(
-                                    Component.text(
-                                            "Distortion information",
-                                            NamedTextColor.GOLD
-                                    )
-                            )
+                            .append(Component.text("Distortion: ", NamedTextColor.GRAY))
+                            .appendNewline()
+                            .append(Component.text(
+                                    String.format("~%.6f blocks/meter", scale),
+                                    NamedTextColor.WHITE
+                            ))
                             .append(Component.newline())
-                            .append(
-                                    Component.text(
-                                            "Scale factor: ",
-                                            NamedTextColor.GRAY
-                                    )
-                            )
-                            .append(
-                                    Component.text(
-                                            String.format("%.6f blocks/meter", distortion),
-                                            NamedTextColor.WHITE
-                                    )
-                            )
-                            .append(Component.newline())
-                            .append(
-                                    Component.text(
-                                            "Angle: ",
-                                            NamedTextColor.GRAY
-                                    )
-                            )
-                            .append(
-                                    Component.text(
-                                            String.format("%.4f°", angle),
-                                            NamedTextColor.WHITE
-                                    )
-                            )
+                            .append(Component.text(
+                                    String.format("± %.4f°", angle),
+                                    NamedTextColor.WHITE
+                            ))
                             .build()
             );
 
         } catch (OutOfProjectionBoundsException e) {
-
             player.sendMessage(
                     Component.text(
-                            "You are outside the bounds of the active projection.",
+                            "You are outside the projection bounds.",
                             NamedTextColor.RED
                     )
             );
-
         }
     }
 }
