d32ffae4409024ba3833f9f678399cbea8f78d18
Zoriot <zoriot@proton.me>
2026-07-06 19:28:22 +0200

    build: transition project to Gradle build system
    Dependencies were also updated where possible.

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    `java-library`
+    alias(libs.plugins.lombok)
+    alias(libs.plugins.plugin.yml)
+}
+
+repositories {
+    mavenCentral()
+    // mavenLocal() - Only use this for testing if ever
+
+    maven("https://maven.smyler.net/releases/")
+
+    exclusiveContent {
+        forRepository {
+            maven {
+                url = uri("https://repo.papermc.io/repository/maven-public/")
+            }
+        }
+        // Ensure papermc repo is only used for paper dependencies
+        // Paper also proxies maven central, which has some issues - see https://github.com/PaperMC/Paper/issues/13987
+        filter {
+            includeGroup("io.papermc")
+            includeGroup("io.papermc.paper")
+            includeGroup("net.md-5")
+            includeGroup("com.mojang")
+        }
+    }
+}
+
+dependencies {
+    paperLibrary(libs.terraminusminus)
+    paperLibrary(libs.daporkchop.lib.common)
+    paperLibrary(libs.bstats)
+    compileOnly(libs.paper.api)
+    // Terra+- itself doesn't need Jackson, but we are hitting this JDK bug: https://bugs.openjdk.org/browse/JDK-8305250.
+    // Having a direct compile dependency on Jackson gets rid of the unnecessary warning.
+    compileOnly(libs.jackson.databind)
+}
+
+group = "de.btegermany"
+version = "1.6.1"
+description = "A plugin which implements the terra-- api in a paper plugin"
+java.sourceCompatibility = JavaVersion.VERSION_21
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType<Javadoc> {
+    options.encoding = "UTF-8"
+}
+
+paper {
+    website = "https://bte-germany.de"
+
+    main = "de.btegermany.terraplusminus.Terraplusminus"
+
+    apiVersion = "1.21.4"
+
+    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
+    authors = listOf("meysster", "Nudlsupp", "Nachwahl", "Zoriot")
+
+    prefix = "T+-"
+
+    loader = "de.btegermany.terraplusminus.PluginLibrariesLoader"
+    generateLibrariesJson = true // https://docs.eldoria.de/pluginyml/libraries/#paper
+}
+
+tasks {
+    generatePaperPluginDescription {
+        useDefaultCentralProxy()
+    }
+}
