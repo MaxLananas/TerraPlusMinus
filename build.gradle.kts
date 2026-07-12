d75d8789b5f3edd01dff19066908445a580f1ec1
Zoriot <zoriot@proton.me>
2026-07-12 21:54:45 +0200

    feat: Add Shadow plugin for relocation because Bstats requires it

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.lombok)
     alias(libs.plugins.plugin.yml)
     alias(libs.plugins.sonarqube)
+    alias(libs.plugins.shadow)
 }
 
 repositories {
@@ -37,7 +38,7 @@ repositories {
 dependencies {
     paperLibrary(libs.terraminusminus)
     paperLibrary(libs.daporkchop.lib.common)
-    paperLibrary(libs.bstats)
+    implementation(libs.bstats)
     paperLibrary(libs.pluginupdater.common) {
         exclude(group = "com.google.guava", module = "guava")
     }
@@ -81,4 +82,21 @@ tasks {
     generatePaperPluginDescription {
         useDefaultCentralProxy()
     }
+}
+
+tasks.jar {
+    archiveClassifier = "UNSHADED"
+    enabled = false // Disable the default jar task since we are using shadowJar
+}
+
+tasks.shadowJar {
+    archiveClassifier = ""
+    relocate(
+        "org.bstats",
+        "de.btegermany.terraplusminus.libs.bstats"
+    )
+}
+
+tasks.assemble {
+    dependsOn(tasks.shadowJar) // Ensure that the shadowJar task runs before the build task
 }
